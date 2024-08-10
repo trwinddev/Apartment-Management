eacc618cc3edd53a4fc1fe9b027b1e5f8a934efd
phongnt <>
2024-08-10 17:32:24 +0700

    install package and setup requirements, settings

---
+++ config/settings/local.py
@@ -0,0 +1,29 @@
+from os import getenv, path
+
+from dotenv import load_dotenv
+
+from .base import *  # noqa
+
+from .base import BASE_DIR
+
+local_env_file = path.join(BASE_DIR, ".envs", ".env.local")
+
+if path.isfile(local_env_file):
+    load_dotenv(local_env_file)
+
+DEBUG = True
+
+SITE_NAME = getenv("SITE_NAME")
+
+SECRET_KEY = getenv(
+    "DJANGO_SECRET_KEY", "q-lxkK-XTLOtsh0_-N81EDanUyInA1KhsvLQMFoCrLfCZ6Gp9rk"
+)
+
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
+
+ADMIN_URL = getenv("DJANGO_ADMIN_URL")
+EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
+EMAIL_HOST = getenv("EMAIL_HOST")
+EMAIL_PORT = getenv("EMAIL_PORT")
+DEFAULT_FROM_EMAIL = getenv("DEFAULT_FROM_EMAIL")
+DOMAIN = getenv("DOMAIN")
---
+++ config/settings/production.py
@@ -0,0 +1,22 @@
+from os import getenv, path
+
+from dotenv import load_dotenv
+
+from .base import *  # noqa
+
+from .base import BASE_DIR
+
+local_env_file = path.join(BASE_DIR, ".envs", ".env.local")
+
+if path.isfile(local_env_file):
+    load_dotenv(local_env_file)
+
+SECRET_KEY = getenv(
+    "DJANGO_SECRET_KEY",
+)
+
+ADMIN_URL = getenv("DJANGO_ADMIN_URL")
+
+ALLOWED_HOSTS = []
+
+ADMINS = [("Ngo Trong Phong","ngotrongphong.dev@gmail.com"),]
